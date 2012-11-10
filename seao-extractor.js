4b6d87d3737ce5520d44c90d73d463c320c57996
Pierre Buyle <buyle@pheromone.ca>
2012-11-10 16:17:13 -0500

    Retrieves notices by category.

--- seao-extractor.js
+++ seao-extractor.js
@@ -1,6 +1,7 @@
 var util = require('util'),
-  fs = require("fs")
- jsdom = require('jsdom');
+ fs = require("fs")
+ jsdom = require('jsdom'),
+ url = require('url');
 
 var jquery = fs.readFileSync("./jquery-1.7.2.min.js").toString();
 
@@ -9,6 +10,54 @@ var jquery = fs.readFileSync("./jquery-1.7.2.min.js").toString();
  * Regular expression that matches all blank spaces. Used for cleaning up HTML.
  */
 var reg_space = /[\s\n\r\t ]+/g;
+
+function getNotices(categoryId, page, pageSize, callback) {
+  var pageNumber = parseInt(page) || 1;
+  var nbResult = parseInt(pageSize) || 100;
+  var cachePath = "./" + [categoryId, nbResult, pageNumber].join('-')  + ".html";
+  //var url = 'http://seao.ca/Recherche/avis_selectionnes_jour.aspx?PageNumber=' + pageNumber + '&CallingPage=4&SubCategoryCode=' + categoryId + '&NbResult=' + nbResult + '&SortOrder=1&SortBy=2';
+  var noticesUrl = 'https://seao.ca/Recherche/avis_selectionnes_jour.aspx?SubCategoryCode=' + categoryId + '&callingPage=4&CatChoosen=1&NbResult=' + nbResult;
+  if (pageNumber > 1) {
+    noticesUrl += '&PageNumber=' + pageNumber;
+    callback(new Error('Paging not supported...'), {page: pageNumber, total: 0, notices: []});
+    return;
+  }
+  jsdom.env({
+    html: noticesUrl,
+    src: [jquery],
+    done: function(errors, window) {
+      if (errors) {
+        callback(errors, {page: pageNumber, total: 0, notices: []});
+      }
+      else {
+        var $ = window.jQuery;
+        var total = parseInt($('#UCSearchResults1_PublishedOpportunityNumResultslbl').text()) || 0;
+        var results = {page: pageNumber, total: total, notices: []};
+        $('#tblResults tr:not(:first-child)').each(function(){
+          var data = $('td', this);
+          var notice = {
+            //url, No, NoRef, Status, Title
+            url: data.eq(1).find('a:first-child').attr('href'),
+            No: data.eq(1).find('a:first-child').text().replace(reg_space, ' ').trim(),
+            Title: data.eq(1).find('.titreAvis').text().replace(reg_space, ' ').trim(),
+            Status: data.eq(2).text().replace(reg_space, ' ').trim()
+          };
+          var matches =  data.eq(1).text().match(/\/\s([0-9]*)\s/);
+          if (matches) {
+            notice.NoRef = matches[1];
+          }
+          notice.itemId = url.parse(notice.url, true).query.ItemId;
+          notice.url = 'https://seao.ca/OpportunityPublication/avisconsultes.aspx?ItemId=' + notice.itemId;
+          notice.information = {
+            PublicationDate: data.eq(3).text()
+          }
+          results.notices.push(notice);
+        });
+        callback(null, results);
+      }
+    }
+  });
+}
 
 function getNotice(itemId, callback) {
   var cachePath = "./" + itemId + ".html",
@@ -22,7 +71,7 @@ function getNotice(itemId, callback) {
         itemId: itemId
       };
       if (errors) {
-        callback(error, notice);
+        callback(errors, notice);
       }
       else {
         var $ = window.jQuery;
@@ -40,10 +89,12 @@ function getNotice(itemId, callback) {
 
         var buyerInfoTable = $('#MainUserControl_lbBlockInformation_buyers').parents('h2').next();
         notice.buyer = {
-          coordinator: $('#MainUserControl_coordinatorsRows td.item', buyerInfoTable).html().replace(reg_space, ' '),
-          website: $('a[href^="http"]:first', buyerInfoTable).attr('href') || null,
+          website: $('a[href^="http"]:first', buyerInfoTable).attr('href') || null
         };
-
+        var coordinatorElement = $('#MainUserControl_coordinatorsRows td.item', buyerInfoTable);
+        if (coordinatorElement.length) {
+          notice.buyer.coordinator = coordinatorElement.html().replace(reg_space, ' ')
+        }
         buyerInfoTable.find('td.item span[id]').each(function() {
           if (this.id) {
             var id = this.id.replace(/(^MainUserControl_lbl?)|(Textvalue$)/, '');
@@ -92,6 +143,10 @@ exports.getNotice = getNotice;
 // 7a94703d-8e78-46e7-924a-ea65ae52ea7c
 // 26644a9c-e575-41fe-9487-c021e91e74b1
 // 98558b11-b359-459d-a275-8958cfcdcd6d
-getNotice('98558b11-b359-459d-a275-8958cfcdcd6d', function(error, notice) {
-  console.log(util.inspect(notice || error, true, null, true));
+//getNotice('a3461598-3fde-4587-a35c-dbe8058792a1', function(error, notice) {
+//  console.log(util.inspect(notice || error, false, null, true));
+//});
+
+getNotices('G15', 1, 10, function(error, results) {
+  console.log(util.inspect(error || results, false, null, true));
 });
