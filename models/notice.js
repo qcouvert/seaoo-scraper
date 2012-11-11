a9d99d982e6b393f9ab453e379a89f82c302a536
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2012-11-11 11:26:36 -0500

    upsert notices

--- models/notice.js
+++ models/notice.js
@@ -4,7 +4,12 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
 module.exports = new Schema({
-    slug: {type:String, index:true},
+    itemId: {type:String, index:true},
+    No: {type:String, index:true},
+    NoRef: {type:String, index:true},
     url: String,
-    
+    Title: String,
+    information: {
+        PublicationDate: String
+    }
 });
