8e7e3901dd15e6866f65d07c560ea9596a611ae6
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2012-11-11 12:29:50 -0500

    don't stop on every errors

--- models/notice.js
+++ models/notice.js
@@ -3,13 +3,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
-module.exports = new Schema({
+var Notice = module.exports = new Schema({
     itemId: {type:String, index:true},
     No: {type:String, index:true},
     NoRef: {type:String, index:true},
     url: String,
     Title: String,
+    Status: String,
     information: {
         PublicationDate: String
-    }
+    },
+    creation: {type:Date, default:Date.now},
+    update: Date,
+    tags: [String]
+});
+
+Notice.index({tags: 1});
+
+Notice.pre('save', function(next) {
+    this.update = Date.now();
 });
